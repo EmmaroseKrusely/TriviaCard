a4cb0d00f72ca5f3da8a5653b53ca0149dc946ed
EmmaroseKrusely <70153909+EmmaroseKrusely@users.noreply.github.com>
2021-04-30 10:17:06 -0400

    Add files via upload

---
+++ CodeTriviaCard/main.js
@@ -0,0 +1,31 @@
+$(document).ready(() =>{
+    $('.hint-box').on('click', () => {
+      $('.hint').slideToggle(700);
+    });
+    
+    $('.wrong-answer-one').on('click', () => {
+      $('.wrong-text-one').fadeOut('slow');
+      $('frown').show();
+    });
+    $('.wrong-answer-two').on('click', () => {
+      $('.wrong-text-two').fadeOut('slow');
+      $('frown').show();
+    });
+    $('.wrong-answer-three').on('click', () => {
+      $('.wrong-text-three').fadeOut('slow');
+      $('frown').show();
+    });
+    
+    $('correct-answer').on('click', () =>{
+      $('frown').hide();
+      $('smiley').show();
+      $('.wrong-answer-one').fadeOut('slow');
+      $('.wrong-answer-two').fadeOut('slow');
+      $('.wrong-answer-three').fadeOut('slow');
+    });
+    
+    
+    
+    
+    
+    });
